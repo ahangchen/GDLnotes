93367f7735696346165170e3f72dc3f1fcdfce45
ahangchen <1418659400@qq.com>
2016-05-25 12:28:20 +0800

    it works!

--- src/neural/nn_overfit.py
+++ src/neural/nn_overfit.py
@@ -74,7 +74,7 @@ def tf_better_nn(offset_range=-1, regular=False, drop_out=False, lrd=False):
         valid_prediction = tf.nn.softmax(valid_predict)
         test_prediction = tf.nn.softmax(test_predict)
 
-    num_steps = 3001
+    num_steps = 30001
 
     with tf.Session(graph=graph) as session:
         tf.initialize_all_variables().run()
@@ -103,25 +103,22 @@ def tf_better_nn(offset_range=-1, regular=False, drop_out=False, lrd=False):
         print("Test accuracy: %.1f%%" % accuracy(test_prediction.eval(), test_labels))
 
 
-def tf_deep_nn(offset_range=-1, regular=False, drop_out=False, lrd=False, layer_cnt=2):
-    batch_size = 256
+def tf_deep_nn(regular=False, drop_out=False, lrd=False, layer_cnt=2):
+    batch_size = 128
 
     graph = tf.Graph()
     with graph.as_default():
-        # Input data. For the training data, we use a placeholder that will be fed
-        # at run time with a training minibatch.
-        tf_train_dataset = tf.placeholder(tf.float32,
-                                          shape=(batch_size, image_size * image_size))
+        tf_train_dataset = tf.placeholder(tf.float32, shape=(batch_size, image_size * image_size))
         tf_train_labels = tf.placeholder(tf.float32, shape=(batch_size, num_labels))
         tf_valid_dataset = tf.constant(valid_dataset)
         tf_test_dataset = tf.constant(test_dataset)
 
         hidden_node_count = 1024
-        # Variables.
-        hidden_stddev = np.sqrt(1.0 / 784)
+        # start weight
+        hidden_stddev = np.sqrt(2.0 / 784)
         weights1 = tf.Variable(tf.truncated_normal([image_size * image_size, hidden_node_count], stddev=hidden_stddev))
         biases1 = tf.Variable(tf.zeros([hidden_node_count]))
-
+        # middle weight
         weights = []
         biases = []
         hidden_cur_cnt = hidden_node_count
@@ -130,53 +127,43 @@ def tf_deep_nn(offset_range=-1, regular=False, drop_out=False, lrd=False, layer_
                 hidden_next_cnt = int(hidden_cur_cnt / 2)
             else:
                 hidden_next_cnt = 2
-            hidden_stddev = np.sqrt(1.0 / hidden_cur_cnt)
-            weights.append(tf.truncated_normal([hidden_cur_cnt, hidden_next_cnt], stddev=hidden_stddev))
+            hidden_stddev = np.sqrt(2.0 / hidden_cur_cnt)
+            weights.append(tf.Variable(tf.truncated_normal([hidden_cur_cnt, hidden_next_cnt], stddev=hidden_stddev)))
             biases.append(tf.Variable(tf.zeros([hidden_next_cnt])))
             hidden_cur_cnt = hidden_next_cnt
-
-        weights2 = tf.Variable(tf.truncated_normal([hidden_cur_cnt, num_labels], stddev=hidden_stddev / 2))
-        biases2 = tf.Variable(tf.zeros([num_labels]))
-
-        # Training computation. right most
+        # first wx + b
         y0 = tf.matmul(tf_train_dataset, weights1) + biases1
+        # first relu
         hidden = tf.nn.relu(y0)
         hidden_drop = hidden
-
-        valid_y0 = tf.matmul(tf_valid_dataset, weights1) + biases1
-        valid_hidden = tf.nn.relu(valid_y0)
-
-        test_y0 = tf.matmul(tf_test_dataset, weights1) + biases1
-        test_hidden = tf.nn.relu(test_y0)
-
-        # enable DropOut
+        # first DropOut
         keep_prob = 0.5
         if drop_out:
             hidden_drop = tf.nn.dropout(hidden, keep_prob)
             hidden_drop = hidden_drop
-        y0s = []
-        y0s.append(y0)
-        y1s = []
+        # first wx+b for valid
+        valid_y0 = tf.matmul(tf_valid_dataset, weights1) + biases1
+        valid_hidden = tf.nn.relu(valid_y0)
+        # first wx+b for test
+        test_y0 = tf.matmul(tf_test_dataset, weights1) + biases1
+        test_hidden = tf.nn.relu(test_y0)
 
-        hiddens = []
-        hiddens.append(hidden)
-        hidden_drops = []
-        hidden_drops.append(hidden_drop)
-        valid_y0s = []
-        valid_y0s.append(valid_y0)
-        valid_hiddens = []
-        valid_hiddens.append(valid_hidden)
-        test_y0s = []
-        test_y0s.append(test_y0)
-        test_hiddens = []
-        test_hiddens.append(test_hidden)
+        # middle value for calculate
+        y0s = [y0]
+        y1s = []
+        hiddens = [hidden]
+        hidden_drops = [hidden_drop]
+        valid_y0s = [valid_y0]
+        valid_hiddens = [valid_hidden]
+        test_y0s = [test_y0]
+        test_hiddens = [test_hidden]
 
         # middle layer
         for i in range(layer_cnt - 2):
             y1s.append(tf.matmul(hidden_drops[i], weights[i]) + biases[i])
             hidden_drops.append(tf.nn.relu(y1s[i]))
             if drop_out:
-                keep_prob += 0.5 * (i + 1) / float(layer_cnt)
+                keep_prob += 0.5 * i / (layer_cnt + 1)
                 hidden_drops[i + 1] = tf.nn.dropout(hidden_drops[i + 1], keep_prob)
 
             y0s.append(tf.matmul(hiddens[i], weights[i]) + biases[i])
@@ -188,8 +175,13 @@ def tf_deep_nn(offset_range=-1, regular=False, drop_out=False, lrd=False, layer_
             test_y0s.append(tf.matmul(test_hiddens[i], weights[i]) + biases[i])
             test_hiddens.append(tf.nn.relu(test_y0s[i + 1]))
 
+        # last weight
+        weights2 = tf.Variable(tf.truncated_normal([hidden_cur_cnt, num_labels], stddev=hidden_stddev / 2))
+        biases2 = tf.Variable(tf.zeros([num_labels]))
+        # last wx + b
         logits = tf.matmul(hidden_drops[layer_cnt - 2], weights2) + biases2
-        # only drop out when train
+
+        # predicts
         logits_predict = tf.matmul(hiddens[layer_cnt - 2], weights2) + biases2
         valid_predict = tf.matmul(valid_hiddens[layer_cnt - 2], weights2) + biases2
         test_predict = tf.matmul(test_hiddens[layer_cnt - 2], weights2) + biases2
@@ -201,42 +193,34 @@ def tf_deep_nn(offset_range=-1, regular=False, drop_out=False, lrd=False, layer_
             for i in range(len(weights)):
                 l2_loss += tf.nn.l2_loss(weights[i])
                 # l2_loss += tf.nn.l2_loss(biases[i])
-            beta = 1.0 / image_size / image_size
+            beta = 0.2 / batch_size
             l2_loss *= beta
-        loss = tf.reduce_mean(
-            tf.nn.softmax_cross_entropy_with_logits(logits, tf_train_labels)) + l2_loss
-        loss *= 10
+        loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits, tf_train_labels)) + l2_loss
+
         # Optimizer.
-        optimizer = tf.train.GradientDescentOptimizer(0.5).minimize(loss)
         if lrd:
-            cur_step = tf.Variable(0)  # count the number of steps taken.
-            starter_learning_rate = 0.1
+            cur_step = tf.Variable(0, trainable=False)  # count the number of steps taken.
+            starter_learning_rate = 0.4
             learning_rate = tf.train.exponential_decay(starter_learning_rate, cur_step, 100000, 0.96, staircase=True)
             optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss, global_step=cur_step)
+        else:
+            optimizer = tf.train.GradientDescentOptimizer(0.5).minimize(loss)
 
         # Predictions for the training, validation, and test data.
         train_prediction = tf.nn.softmax(logits_predict)
         valid_prediction = tf.nn.softmax(valid_predict)
         test_prediction = tf.nn.softmax(test_predict)
 
-    num_steps = 40001
+    num_steps = 3001
 
     with tf.Session(graph=graph) as session:
         tf.initialize_all_variables().run()
         print("Initialized")
         for step in range(num_steps):
-            # Pick an offset within the training data, which has been randomized.
-            # Note: we could use better randomization across epochs.
-            if offset_range == -1:
-                offset_range = train_labels.shape[0] - batch_size
-
+            offset_range = train_labels.shape[0] - batch_size
             offset = (step * batch_size) % offset_range
-            # Generate a minibatch.
             batch_data = train_dataset[offset:(offset + batch_size), :]
             batch_labels = train_labels[offset:(offset + batch_size), :]
-            # Prepare a dictionary telling the session where to feed the minibatch.
-            # The key of the dictionary is the placeholder node of the graph to be fed,
-            # and the value is the numpy array to feed to it.
             feed_dict = {tf_train_dataset: batch_data, tf_train_labels: batch_labels}
             _, l, predictions = session.run(
                 [optimizer, loss, train_prediction], feed_dict=feed_dict)
@@ -247,7 +231,6 @@ def tf_deep_nn(offset_range=-1, regular=False, drop_out=False, lrd=False, layer_
                     valid_prediction.eval(), valid_labels))
         print("Test accuracy: %.1f%%" % accuracy(test_prediction.eval(), test_labels))
 
-
 if __name__ == '__main__':
     image_size = 28
     num_labels = 10
@@ -257,5 +240,5 @@ if __name__ == '__main__':
     # tf_better_nn(offset_range=1000)
     # tf_better_nn(offset_range=1000, drop_out=True)
     # tf_better_nn(lrd=True)
-    tf_deep_nn(layer_cnt=4, lrd=False, drop_out=False, regular=False)
-    # tf_deep_nn(layer_cnt=4)
+    tf_deep_nn(layer_cnt=5, lrd=True, drop_out=True, regular=True)
+    # tf_deep_nn(layer_cnt=3)
