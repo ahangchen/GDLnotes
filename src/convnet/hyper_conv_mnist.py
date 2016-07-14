6f7fa2dacbf37c448f718e25f77f95b707d26ebb
cwh <cweihang@foxmail.com>
2016-07-14 21:34:01 +0800

    dynamic convnet

--- src/convnet/hyper_conv_mnist.py
+++ src/convnet/hyper_conv_mnist.py
@@ -1,10 +1,28 @@
-import tensorflow as tf
-
 from convnet.conv_mnist import maxpool2d, load_reformat_not_mnist
 from neural.full_connect import accuracy
 
+import tensorflow as tf
 
-def conv_train(basic_hps, stride_p, drop=False, lrd=False):
+
+def up_div(y, x):
+    if y % x > 0:
+        return y / x + 1
+    else:
+        return y / x
+
+
+def size_by_conv(stride_ps, data_size, total_layer_cnt):
+    param1 = data_size[1]
+    param2 = data_size[2]
+    for i in range(total_layer_cnt):
+        param1 = up_div(param1, stride_ps[i][1])
+        param1 = up_div(param1, 2)
+        param2 = up_div(param2, stride_ps[i][2])
+        param2 = up_div(param2, 2)
+    return param1 * param2 * data_size[0]
+
+
+def conv_train(basic_hps, stride_ps, layer_cnt=3, drop=False, lrd=False):
     batch_size = basic_hps['batch_size']
     patch_size = basic_hps['patch_size']
     depth = basic_hps['depth']
@@ -25,12 +43,13 @@ def conv_train(basic_hps, stride_p, drop=False, lrd=False):
             [patch_size, patch_size, num_channels, depth], stddev=0.1))
         input_biases = tf.Variable(tf.zeros([depth]))
 
-        layer2_weights = tf.Variable(tf.truncated_normal(
-            [patch_size, patch_size, depth, depth], stddev=0.1))
-        layer2_biases = tf.Variable(tf.constant(1.0, shape=[depth]))
+        mid_layer_cnt = layer_cnt - 1
+        layer_weights = [tf.Variable(tf.truncated_normal(
+            [patch_size, patch_size, depth, depth], stddev=0.1)) for _ in range(mid_layer_cnt)]
+        layer_biases = [tf.Variable(tf.constant(1.0, shape=[depth])) for _ in range(mid_layer_cnt)]
 
-        output_weights = tf.Variable(tf.truncated_normal(
-            [64, num_hidden], stddev=0.1))
+        output_size = size_by_conv(stride_ps, [batch_size, image_size, image_size, num_channels], layer_cnt)
+        output_weights = tf.Variable(tf.truncated_normal([output_size, num_hidden], stddev=0.1))
         output_biases = tf.Variable(tf.constant(1.0, shape=[num_hidden]))
         final_weights = tf.Variable(tf.truncated_normal(
             [num_hidden, num_labels], stddev=0.1))
@@ -38,20 +57,22 @@ def conv_train(basic_hps, stride_p, drop=False, lrd=False):
 
         # Model.
         def model(data):
-            conv = tf.nn.conv2d(data, input_weights, stride_p, padding='SAME')
+            conv = tf.nn.conv2d(data, input_weights, stride_ps[0], use_cudnn_on_gpu=True, padding='SAME')
             conv = maxpool2d(conv)
             hidden = tf.nn.relu(conv + input_biases)
             if drop:
                 hidden = tf.nn.dropout(hidden, 0.5)
-
-            conv = tf.nn.conv2d(hidden, layer2_weights, stride_p, padding='SAME')
-            conv = maxpool2d(conv)
-            hidden = tf.nn.relu(conv + layer2_biases)
-            if drop:
-                hidden = tf.nn.dropout(hidden, 0.7)
+            for i in range(mid_layer_cnt):
+                print i
+                conv = tf.nn.conv2d(hidden, layer_weights[i], stride_ps[i + 1], use_cudnn_on_gpu=True, padding='SAME')
+                conv = maxpool2d(conv)
+                hidden = tf.nn.relu(conv + layer_biases[i])
+                if drop:
+                    hidden = tf.nn.dropout(hidden, 0.7)
 
             shape = hidden.get_shape().as_list()
-            reshape = tf.reshape(hidden, [shape[0], shape[1] * shape[2] * shape[3]])
+            reshape = tf.reshape(hidden, [shape[0], output_size])
+
             hidden = tf.nn.relu(tf.matmul(reshape, output_weights) + output_biases)
             if drop:
                 hidden = tf.nn.dropout(hidden, 0.8)
@@ -99,6 +120,13 @@ if __name__ == '__main__':
     num_labels = 10
     train_dataset, train_labels, valid_dataset, valid_labels, test_dataset, test_labels = \
         load_reformat_not_mnist(image_size, num_labels, 1)
+    pick_size = 2048
+    # piece of valid dataset to avoid OOM
+    valid_dataset = valid_dataset[0: pick_size, :, :, :]
+    valid_labels = valid_labels[0: pick_size, :]
+    # piece of test dataset to avoid OOM
+    test_dataset = test_dataset[0: pick_size, :, :, :]
+    test_labels = test_labels[0: pick_size, :]
     # conv_max_pool_train()
     # conv_train()
     basic_hypers = {
@@ -108,5 +136,7 @@ if __name__ == '__main__':
         'num_hidden': 64,
         'num_channels': 1,
     }
-    stride_param = [1, 3, 2, 1]
-    conv_train(basic_hypers, stride_param, lrd=True)
+    layer_sum = 3
+    stride_params = [[1, 1, 1, 1] for _ in range(layer_sum - 1)]
+    stride_params.append([1, 2, 2, 1])
+    conv_train(basic_hypers, stride_params, layer_cnt=layer_sum, lrd=True)
