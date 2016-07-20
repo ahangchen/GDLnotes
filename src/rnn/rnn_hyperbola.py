16e0f8cad8d5f0ce96bbc049c349194e955a9219
ahangchen <1418659400@qq.com>
2016-07-20 23:42:00 +0800

    init for skflow rnn

--- src/rnn/rnn_hyperbola.py
+++ src/rnn/rnn_hyperbola.py
@@ -1,187 +1,82 @@
 # coding=utf-8
-import random
-import numpy as np
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from sklearn import metrics, preprocessing
+
 import tensorflow as tf
+from tensorflow.contrib import learn
+import random
 
 
-def train_data():
-    return [1.0 / (i + random.randint(0, 100)) for i in range(100000)]
+def raw_data():
+    return [1.0 / (i + random.randint(0, 100)) for i in range(1000000)]
 
 
-def logprob(predictions, labels):
-    # prevent negative probability
-    """Log-probability of the true labels in a predicted batch."""
-    predictions[predictions < 1e-10] = 1e-10
-    return np.sum(np.multiply(labels, -np.log(predictions))) / labels.shape[0]
+def piece_data(raw_data, i, piece_size):
+    return raw_data[piece_size * i: piece_size * (i + 1)]
 
 
-def sample_distribution(distribution):
-    """Sample one element from a distribution assumed to be an array of normalized
-    probabilities.
-    """
-    # 取一部分数据用于评估，所取数据比例随机
-    r = random.uniform(0, 1)
-    s = 0
-    for i in range(len(distribution)):
-        s += distribution[i]
-        if s >= r:
-            return i
-    return len(distribution) - 1
+def piece_label(raw_data, i, piece_size):
+    return raw_data[piece_size] * (i + 1)
 
 
-def sample(prediction):
-    """Turn a (column) prediction into 1-hot encoded samples."""
-    p = np.zeros(shape=[1, vocabulary_size], dtype=np.float)
-    p[0, sample_distribution(prediction[0])] = 1.0
-    return p
+def data_idx():
+    return [i for i in range(900000)], [j for j + 900000 in range(100000)]
+
+# Parameters
+learning_rate = 0.1
+training_steps = 3000
+batch_size = 128
+
+# Network Parameters
+n_input = 28  # MNIST data input (img shape: 28*28)
+n_steps = 28  # timesteps
+n_hidden = 128  # hidden layer num of features
+n_classes = 10  # MNIST total classes (0-9 digits)
+
+# hyperbola data
+raw = raw_data()
+train_idxs, test_idxs = data_idx()
+X_train = [piece_data(raw, i, n_input) for i in train_idxs]
+y_train = [piece_label(raw, i, n_input) for i in train_idxs]
+X_test = [piece_data(raw, i, n_input) for i in test_idxs]
+y_test = [piece_label(raw, i, n_input) for i in test_idxs]
+
+# It's useful to scale to ensure Stochastic Gradient Descent will do the right thing
+scaler = preprocessing.StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.fit_transform(X_test)
 
 
-def random_distribution():
-    """Generate a random column of probabilities."""
-    b = np.random.uniform(0.0, 1.0, size=[1, vocabulary_size])
-    return b / np.sum(b, 1)[:, None]
+def rnn_model(X, y):
+    X = tf.reshape(X, [-1, n_steps, n_input])  # (batch_size, n_steps, n_input)
+    # # permute n_steps and batch_size
+    X = tf.transpose(X, [1, 0, 2])
+    # # Reshape to prepare input to hidden activation
+    X = tf.reshape(X, [-1, n_input])  # (n_steps*batch_size, n_input)
+    # # Split data because rnn cell needs a list of inputs for the RNN inner loop
+    X = tf.split(0, n_steps, X)  # n_steps * (batch_size, n_input)
+
+    # Define a GRU cell with tensorflow
+    lstm_cell = tf.nn.rnn_cell.BasicLSTMCell(n_hidden)
+    # Get lstm cell output
+    _, encoding = tf.nn.rnn(lstm_cell, X, dtype=tf.float32)
+
+    return learn.models.logistic_regression(encoding, y)
 
 
-# Simple LSTM Model.
-num_nodes = 64
+classifier = learn.TensorFlowEstimator(model_fn=rnn_model, n_classes=n_classes,
+                                       batch_size=batch_size,
+                                       steps=training_steps,
+                                       learning_rate=learning_rate)
 
-graph = tf.Graph()
-with graph.as_default():
-    # Parameters:
-    # Input gate: input, previous output, and bias.
-    ix = tf.Variable(tf.truncated_normal([vocabulary_size, num_nodes], -0.1, 0.1))
-    im = tf.Variable(tf.truncated_normal([num_nodes, num_nodes], -0.1, 0.1))
-    ib = tf.Variable(tf.zeros([1, num_nodes]))
-    # Forget gate: input, previous output, and bias.
-    fx = tf.Variable(tf.truncated_normal([vocabulary_size, num_nodes], -0.1, 0.1))
-    fm = tf.Variable(tf.truncated_normal([num_nodes, num_nodes], -0.1, 0.1))
-    fb = tf.Variable(tf.zeros([1, num_nodes]))
-    # Memory cell: input, state and bias.
-    cx = tf.Variable(tf.truncated_normal([vocabulary_size, num_nodes], -0.1, 0.1))
-    cm = tf.Variable(tf.truncated_normal([num_nodes, num_nodes], -0.1, 0.1))
-    cb = tf.Variable(tf.zeros([1, num_nodes]))
-    # Output gate: input, previous output, and bias.
-    ox = tf.Variable(tf.truncated_normal([vocabulary_size, num_nodes], -0.1, 0.1))
-    om = tf.Variable(tf.truncated_normal([num_nodes, num_nodes], -0.1, 0.1))
-    ob = tf.Variable(tf.zeros([1, num_nodes]))
-    # Variables saving state across unrollings.
-    saved_output = tf.Variable(tf.zeros([batch_size, num_nodes]), trainable=False)
-    saved_state = tf.Variable(tf.zeros([batch_size, num_nodes]), trainable=False)
-    # Classifier weights and biases.
-    w = tf.Variable(tf.truncated_normal([num_nodes, vocabulary_size], -0.1, 0.1))
-    b = tf.Variable(tf.zeros([vocabulary_size]))
-
-    # Definition of the cell computation.
-    def lstm_cell(i, o, state):
-        """Create a LSTM cell. See e.g.: http://arxiv.org/pdf/1402.1128v1.pdf
-        Note that in this formulation, we omit the various connections between the
-        previous state and the gates."""
-        input_gate = tf.sigmoid(tf.matmul(i, ix) + tf.matmul(o, im) + ib)
-        forget_gate = tf.sigmoid(tf.matmul(i, fx) + tf.matmul(o, fm) + fb)
-        update = tf.matmul(i, cx) + tf.matmul(o, cm) + cb
-        state = forget_gate * state + input_gate * tf.tanh(update)
-        output_gate = tf.sigmoid(tf.matmul(i, ox) + tf.matmul(o, om) + ob)
-        return output_gate * tf.tanh(state), state
-
-    # Input data.
-    train_data = list()
-    for _ in range(num_unrollings + 1):
-        train_data.append(
-            tf.placeholder(tf.float32, shape=[batch_size, vocabulary_size]))
-    train_inputs = train_data[:num_unrollings]
-    train_labels = train_data[1:]  # labels are inputs shifted by one time step.
-
-    # Unrolled LSTM loop.
-    outputs = list()
-    output = saved_output
-    state = saved_state
-    for i in train_inputs:
-        output, state = lstm_cell(i, output, state)
-        outputs.append(output)
-
-    # State saving across unrollings.
-    with tf.control_dependencies([saved_output.assign(output),
-                                  saved_state.assign(state)]):
-        # Classifier.
-        logits = tf.nn.xw_plus_b(tf.concat(0, outputs), w, b)
-        loss = tf.reduce_mean(
-            tf.nn.softmax_cross_entropy_with_logits(
-                logits, tf.concat(0, train_labels)))
-
-    # Optimizer.
-    global_step = tf.Variable(0)
-    learning_rate = tf.train.exponential_decay(
-        10.0, global_step, 5000, 0.1, staircase=True)
-    optimizer = tf.train.GradientDescentOptimizer(learning_rate)
-    gradients, v = zip(*optimizer.compute_gradients(loss))
-    gradients, _ = tf.clip_by_global_norm(gradients, 1.25)
-    optimizer = optimizer.apply_gradients(
-        zip(gradients, v), global_step=global_step)
-
-    # Predictions.
-    train_prediction = tf.nn.softmax(logits)
-
-    # Sampling and validation eval: batch 1, no unrolling.
-    sample_input = tf.placeholder(tf.float32, shape=[1, vocabulary_size])
-    saved_sample_output = tf.Variable(tf.zeros([1, num_nodes]))
-    saved_sample_state = tf.Variable(tf.zeros([1, num_nodes]))
-    reset_sample_state = tf.group(
-        saved_sample_output.assign(tf.zeros([1, num_nodes])),
-        saved_sample_state.assign(tf.zeros([1, num_nodes])))
-    sample_output, sample_state = lstm_cell(
-        sample_input, saved_sample_output, saved_sample_state)
-    with tf.control_dependencies([saved_sample_output.assign(sample_output),
-                                  saved_sample_state.assign(sample_state)]):
-        sample_prediction = tf.nn.softmax(tf.nn.xw_plus_b(sample_output, w, b))
-
-num_steps = 7001
-summary_frequency = 100
-
-with tf.Session(graph=graph) as session:
-    tf.initialize_all_variables().run()
-    print('Initialized')
-    mean_loss = 0
-    for step in range(num_steps):
-        batches = train_batches.next()
-        feed_dict = dict()
-        for i in range(num_unrollings + 1):
-            feed_dict[train_data[i]] = batches[i]
-        _, l, predictions, lr = session.run(
-            [optimizer, loss, train_prediction, learning_rate], feed_dict=feed_dict)
-        mean_loss += l
-        if step % summary_frequency == 0:
-            if step > 0:
-                mean_loss /= summary_frequency
-            # The mean loss is an estimate of the loss over the last few batches.
-            print(
-                'Average loss at step %d: %f learning rate: %f' % (step, mean_loss, lr))
-            mean_loss = 0
-            labels = np.concatenate(list(batches)[1:])
-            print('Minibatch perplexity: %.2f' % float(
-                np.exp(logprob(predictions, labels))))
-            if step % (summary_frequency * 10) == 0:
-                # Generate some samples.
-                print('=' * 80)
-                for _ in range(5):
-                    feed = sample(random_distribution())
-                    sentence = characters(feed)[0]
-                    reset_sample_state.run()
-                    for _ in range(79):
-                        prediction = sample_prediction.eval({sample_input: feed})
-                        feed = sample(prediction)
-                        sentence += characters(feed)[0]
-                    print(sentence)
-                print('=' * 80)
-            # Measure validation set perplexity.
-            reset_sample_state.run()
-            valid_logprob = 0
-            for _ in range(valid_size):
-                b = valid_batches.next()
-                predictions = sample_prediction.eval({sample_input: b[0]})
-                valid_logprob = valid_logprob + logprob(predictions, b[1])
-            print('Validation set perplexity: %.2f' % float(np.exp(
-                valid_logprob / valid_size)))
+classifier.fit(X_train, y_train, logdir="/tmp/mnist_rnn")
+score = metrics.accuracy_score(y_test, classifier.predict(X_test))
+print('Accuracy: {0:f}'.format(score))
 
 if __name__ == '__main__':
-    inputs, labels = train_data()
-    dig_nn(inputs, labels, 100, 2, 3)
+    # inputs, labels = train_data()
+    # dig_nn(inputs, labels, 100, 2, 3)
+    pass
