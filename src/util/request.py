137da83d274f71775e833c0d5e5738c8d1d045a5
ahangchen <1418659400@qq.com>
2016-09-29 22:48:22 +0800

    init half trends model

--- src/util/request.py
+++ src/util/request.py
@@ -8,6 +8,7 @@ fit_url = 'http://127.0.0.1:8000/fit/'
 fit_trend_url = 'http://127.0.0.1:8000/fit/trend/'
 better_hp_trend_url = 'http://127.0.0.1:8000/fit/trend/'
 hp2trend_url = 'http://127.0.0.1:8000/hp2trend/'
+half_trend_url = 'http://127.0.0.1:8000/half_trend/'
 fit2_url = 'http://127.0.0.1:8000/fit2/'
 better_hp_url = 'http://127.0.0.1:8000/hyper/'
 predict_loss_url = 'http://127.0.0.1:8000/predict/'
@@ -64,6 +65,16 @@ def fit_trend(hypers, loss_es):
 def better_trend_hyper(hypers, loss):
     data = urllib.urlencode({'hyper': hypers, 'loss': loss})
     req = urllib2.Request(hp2trend_url, data)
+    response = urllib2.urlopen(req)
+    res = json.loads(response.read())
+    better_hypers = res['msg']
+    # print(res)
+    return better_hypers
+
+
+def half_trend_hyper(hypers, loss):
+    data = urllib.urlencode({'hyper': hypers, 'loss': loss})
+    req = urllib2.Request(half_trend_url, data)
     response = urllib2.urlopen(req)
     res = json.loads(response.read())
     better_hypers = res['msg']
